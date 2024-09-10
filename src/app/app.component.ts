b13be8747e3b6168f3ec12d811f980f7510f7a44
Wenying Chang <changwenyingnl@gmail.com>
2024-09-10 14:39:48 +0200

    feat:/add file structure and navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, NavbarComponent],
+  template:`
+    <main class="main">
+      <app-navbar></app-navbar>
+    </main>
+    <router-outlet />
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
