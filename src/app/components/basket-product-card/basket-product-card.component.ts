91b67c27d68e554e9876e1463a09284e1e0c4fcb
Wenying Chang <changwenyingnl@gmail.com>
2024-09-11 16:55:56 +0200

    add ui for the basket page

--- src/app/components/basket-product-card/basket-product-card.component.ts
+++ src/app/components/basket-product-card/basket-product-card.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-basket-product-card',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatButtonModule, MatIconModule, MatSelectModule],
   templateUrl: './basket-product-card.component.html',
   styleUrl: './basket-product-card.component.scss'
 })
